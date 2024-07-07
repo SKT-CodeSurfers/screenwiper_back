781ea0e5d83271621bb600a89ef9b7425606f29e
SongGaHyeon <gahyunsong@hufs.ac.kr>
2024-07-07 20:11:53 +0900

    back initial pack

---
+++ screenwiper/src/test/java/com/example/screenwiper/ScreenwiperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.screenwiper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScreenwiperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
