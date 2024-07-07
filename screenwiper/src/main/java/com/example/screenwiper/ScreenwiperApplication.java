b7d3a033640651f62e95d475e308d709b73698a5
SongGaHyeon <gahyunsong@hufs.ac.kr>
2024-07-07 20:11:53 +0900

    back initial pack

---
+++ screenwiper/src/main/java/com/example/screenwiper/ScreenwiperApplication.java
@@ -0,0 +1,13 @@
+package com.example.screenwiper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenwiperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenwiperApplication.class, args);
+	}
+
+}
