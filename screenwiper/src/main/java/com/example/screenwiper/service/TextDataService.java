410334c9b73740a7bb5e3994c8ea532421fb737f
chojayoung <26006@naver.com>
2024-08-27 02:52:45 +0900

    Modify : TextDataService 수정

--- screenwiper/src/main/java/com/example/screenwiper/service/TextDataService.java
+++ screenwiper/src/main/java/com/example/screenwiper/service/TextDataService.java
@@ -4,7 +4,6 @@ import com.example.screenwiper.domain.TextData;
 import com.example.screenwiper.repository.TextDataRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
@@ -14,9 +13,7 @@ public class TextDataService {
     @Autowired
     private TextDataRepository textDataRepository;
 
-    public Page<TextData> getTextDataList(String type, int page, int size) {
-        Pageable pageable = PageRequest.of(page, size);
-
+    public Page<TextData> getTextDataList(String type, Pageable pageable) {
         if (type != null && !type.isEmpty()) {
             return textDataRepository.findByCategoryName(type, pageable);
         } else {
