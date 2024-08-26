3a615a72ff4e1ceafe5d4820f33995fe4a8824b9
chojayoung <26006@naver.com>
2024-08-27 02:20:51 +0900

    Add : TextDataRepository에 import 문 추가

--- screenwiper/src/main/java/com/example/screenwiper/repository/TextDataRepository.java
+++ screenwiper/src/main/java/com/example/screenwiper/repository/TextDataRepository.java
@@ -1,7 +1,12 @@
 package com.example.screenwiper.repository;
 
 import com.example.screenwiper.domain.TextData;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 
 public interface TextDataRepository extends JpaRepository<TextData, Long> {
+    Page<TextData> findByCategoryName(String categoryName, Pageable pageable);
 }
