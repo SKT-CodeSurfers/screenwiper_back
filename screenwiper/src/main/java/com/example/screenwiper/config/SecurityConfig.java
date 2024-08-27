e3dcdd3aaa49d7fc04d1d38b9e7d1ee96b357d34
SongGaHyeon <gahyunsong@hufs.ac.kr>
2024-08-27 17:32:36 +0900

    security config modify

--- screenwiper/src/main/java/com/example/screenwiper/config/SecurityConfig.java
+++ screenwiper/src/main/java/com/example/screenwiper/config/SecurityConfig.java
@@ -4,6 +4,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 public class SecurityConfig {
@@ -20,5 +25,18 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable());  // CSRF 비활성화 (필요에 따라 설정)
 
         return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://15.164.115.105:8080")); // 외부 API 출처 허용
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE")); // 허용할 HTTP 메서드
+        configuration.setAllowCredentials(true); // 자격 증명 허용 (필요한 경우)
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration); // 모든 경로에 대해 CORS 설정 적용
+        return source;
     }
 }
