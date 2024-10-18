5d6b1d9e32761e855c0724b8cc14a91eb8c407d3
chojayoung <26006@naver.com>
2024-10-18 17:05:56 +0900

    Add : 카카오 로그인 관련 controller 파일 추가

---
+++ screenwiper/src/main/java/com/example/screenwiper/controller/KakaoAuthController.java
@@ -0,0 +1,24 @@
+package com.example.screenwiper.controller;
+
+import com.example.screenwiper.dto.JwtTokenResponseDto;
+import com.example.screenwiper.dto.KakaoTokenRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class KakaoAuthController {
+
+    private final KakaoAuthService kakaoAuthService;
+
+    @PostMapping("/kakao/login")
+    public ResponseEntity<JwtTokenResponseDto> kakaoLogin(@RequestBody KakaoTokenRequestDto tokenRequest) {
+        JwtTokenResponseDto jwtTokenResponse = kakaoAuthService.kakaoLogin(tokenRequest.getAuthorizationCode());
+        return ResponseEntity.ok(jwtTokenResponse);
+    }
+}
