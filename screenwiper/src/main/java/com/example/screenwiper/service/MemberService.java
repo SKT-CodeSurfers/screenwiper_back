1d55a1f39f9953572efb5cbf47351b8c082130fc
chojayoung <26006@naver.com>
2024-10-27 20:55:00 +0900

    Add : MemberService 추가

--- screenwiper/src/main/java/com/example/screenwiper/service/MemberService.java
+++ screenwiper/src/main/java/com/example/screenwiper/service/MemberService.java
@@ -1,52 +1,29 @@
 package com.example.screenwiper.service;
 
+import com.example.screenwiper.dto.MemberDto;
 import com.example.screenwiper.domain.Member;
 import com.example.screenwiper.repository.MemberRepository;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
-@Slf4j
+import java.util.Optional;
+
 @Service
-@Transactional(readOnly = true)
-@RequiredArgsConstructor
 public class MemberService {
 
     private final MemberRepository memberRepository;
 
-    @Transactional(readOnly = false)
-    public Long join(Member member) {
-        memberRepository.save(member);
-        return member.getId();
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
-    public boolean isMemberExist(Long id) {
-        // findById 메소드를 사용하여 Optional<Member>를 반환
-        return memberRepository.findById(id).isPresent();
-    }
-
-    @Transactional(readOnly = false)
-    public Member login(Long id) {
-        // findById 메소드를 사용하여 Optional<Member>를 반환
-        return memberRepository.findById(id).orElse(null);
-    }
-
-    @Transactional
-    public boolean deleteMember(Long memberId) {
-        // findById 메소드를 사용하여 Optional<Member>를 반환
-        Member member = memberRepository.findById(memberId).orElse(null);
-        if (member != null) {
-            memberRepository.delete(member);
-            return true;
+    public MemberDto getMemberById(Long memberId) {
+        Optional<Member> memberOptional = memberRepository.findById(memberId);
+        if (memberOptional.isPresent()) {
+            Member member = memberOptional.get();
+            // Member 객체를 MemberDto로 변환
+            return new MemberDto(member.getId(), member.getName(), member.getBirthdate(), member.getNickname(), member.getEmail(), member.isEnabled());
         } else {
-            return false;
+            throw new RuntimeException("회원 정보를 찾을 수 없습니다.");
         }
-    }
-
-    // 기존 회원의 액세스 토큰 업데이트
-    @Transactional(readOnly = false)
-    public void update(Member member) {
-        memberRepository.save(member); // 엔티티를 업데이트
     }
 }
