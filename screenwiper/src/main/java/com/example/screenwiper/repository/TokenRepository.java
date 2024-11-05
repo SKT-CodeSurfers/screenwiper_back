af27d8600290c93b3417305fd3159fbbe6a3fe79
chojayoung <26006@naver.com>
2024-11-05 14:30:47 +0900

    Add : repository 어노테이션 추가

--- screenwiper/src/main/java/com/example/screenwiper/repository/TokenRepository.java
+++ screenwiper/src/main/java/com/example/screenwiper/repository/TokenRepository.java
@@ -2,9 +2,11 @@ package com.example.screenwiper.repository;
 
 import com.example.screenwiper.domain.Token;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface TokenRepository extends JpaRepository<Token, Long> {
     Optional<Token> findByMemberId(Long memberId);
 }
