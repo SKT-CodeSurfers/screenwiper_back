b515989c1d40a47bfcf6fe5dd63b7d4fa2b8ce9f
chojayoung <26006@naver.com>
2024-11-05 14:15:56 +0900

    Add : ScreenwiperApplication 에 jpa 의존성 주입

--- screenwiper/src/main/java/com/example/screenwiper/ScreenwiperApplication.java
+++ screenwiper/src/main/java/com/example/screenwiper/ScreenwiperApplication.java
@@ -2,8 +2,10 @@ package com.example.screenwiper;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.example.screenwiper.repository") // 추가된 부분
 public class ScreenwiperApplication {
 
 	public static void main(String[] args) {
